35b77f4452d49c924f2b9d7a3a1727c286f82a09
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-06-03 22:09:11 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution here!
+const cats=["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph")
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(){
+    cats.pop("Garfield")
+}
+function destructivelyRemoveFirstCat(){
+   cats.shift("Milo") 
+}
+function appendCat(name){
+   
+    const newArray=[...cats, "Broom"]
+    return newArray
+}
+function  prependCat(name){
+    const newArray=["Arnold", ...cats]
+    return newArray
+}
+function  removeLastCat(){
+    const newArray = cats.slice(0,  cats.length-1)
+    return newArray
+}
+function  removeFirstCat(){
+    const newArray = cats.slice(1)
+    return newArray
+}
